0b2d82f5538010ebd04d8271f39accb9776e5850
Aman Ibrahim <amanmibra@gmail.com>
2018-02-06 12:04:09 -0500

    Added api namespace and resources to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :shifts do
+        resource :user
+      end
+      resources :teams do
+        resource :captain
+      end
+      resources :users, :captains
+    end
+  end
+  end
 end
