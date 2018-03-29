d40bd7a30dc59b29c38b987157f0f37429d798ea
amanmibra <amanmibra@gmail.com>
2018-03-29 17:46:42 -0400

    Added more styling for Login page

--- app/javascript/src/containers/Login.jsx
+++ app/javascript/src/containers/Login.jsx
@@ -5,43 +5,58 @@ import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 
 // semantic ui components
-import { Input, Button, Icon, Container } from 'semantic-ui-react';
+import { Input, Button, Icon, Container, Card } from 'semantic-ui-react';
 
 class Login extends Component {
+  renderLoginFields = () => (
+    <div>
+      <Input placeholder="Username" />
+
+      <br />
+      <br />
+
+      <Input placeholder="Password" type="password" />
+
+      <br />
+      <br />
+
+      <Button
+        icon
+        color="green"
+        labelPosition="right"
+      >
+        <Icon name="checkmark" />
+        Sign In!
+      </Button>
+
+      <br />
+      <br />
+
+      <Button
+        icon
+        color="blue"
+        labelPosition="right"
+      >
+        <Icon name="signup" />
+        Sign Up!
+      </Button>
+    </div>
+  );
+
   render() {
     return (
-      <div>
+      <div className="login">
         <Container textalign="center" >
-          <Input placeholder="Username" />
-
-          <br />
-          <br />
-
-          <Input placeholder="Password" type="password" />
-
-          <br />
-          <br />
-
-          <Button
-            icon
-            color="green"
-            labelPosition="right"
-          >
-            <Icon name="checkmark" />
-            Sign In!
-          </Button>
-
-          <br />
-          <br />
-
-          <Button
-            icon
-            color="blue"
-            labelPosition="right"
-          >
-            <Icon name="signup" />
-            Sign Up!
-          </Button>
+          <Card centered fluid color="blue" className="login-card" >
+            <Card.Content>
+              <Card.Header>
+                ⛺⛺ Welcome to your K-Ville Scheduler! ⛺⛺
+              </Card.Header>
+            </Card.Content>
+            <Card.Content>
+              { this.renderLoginFields() }
+            </Card.Content>
+          </Card>
         </Container>
       </div>
     );
