2c6032610a13165594307910d3f319d93a8828ba
Aman Ibrahim <amanmibra@gmail.com>
2018-05-02 19:43:15 -0400

    Added API endpoint to create teams

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -6,6 +6,21 @@ class Api::V1::TeamsController < ApiController
   def index
   end
 
+  def create
+    validate_params
+    @team = Team.create!(
+      name: @name,
+      captain_id: @captain_id,
+      tent_number: @tent_number,
+      tent_type: @tent_type,
+    )
+    if @team.save
+      render json: { status: 'SUCCESS', message: 'Team saved', data: @team }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Team not saved', data: @team.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def set_user
@@ -14,5 +29,13 @@ class Api::V1::TeamsController < ApiController
       else
         @Teams = Team.all
       end
+    end
+
+    def validate_params
+      params.require([:name, :captain_id, :tent_number, :tent_type])
+      @name = params[:name]
+      @captain_id = params[:captain_id]
+      @tent_number = params[:tent_number]
+      @tent_type = params[:tent_type]
     end
 end
