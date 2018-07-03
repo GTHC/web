25eda8a7e32a10f461a2f00ebeb1d96d1874f3d7
Aman Ibrahim <amanmibra@gmail.com>
2018-07-03 08:37:55 -0700

    using calendar comp from docs

--- app/javascript/src/containers/Calendar.jsx
+++ app/javascript/src/containers/Calendar.jsx
@@ -14,6 +14,28 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const Cal = BigCalendar.momentLocalizer(moment);
 
+const event = [{
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1),
+  },
+  {
+    id: 1,
+    title: 'Long Event',
+    start: new Date(2015, 3, 7),
+    end: new Date(2015, 3, 10),
+  },
+  {
+    id: 2,
+    title: 'DTS STARTS',
+    start: new Date(2016, 2, 13, 0, 0, 0),
+    end: new Date(2016, 2, 20, 0, 0, 0),
+  },
+];
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
 class Calendar extends Component {
   render () {
     return (
@@ -21,10 +43,12 @@ class Calendar extends Component {
         <NavBar />
         <div className="body">
           Calendar
-          <Cal
-            events={{}}
-            startAccessor='startDate'
-            endAccessor='endDate'
+          <BigCalendar
+            events={events}
+            views={allViews}
+            step={60}
+            showMultiDayTimes
+            defaultDate={new Date(2015, 3, 1)}
           />
         </div>
       </div>
