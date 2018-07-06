885a757b1eb110eacf3dc8face19debb9278be29
Aman Ibrahim <amanmibra@gmail.com>
2018-07-06 14:59:09 -0700

    Set up scripts

--- bin/db-setup.sh
+++ bin/db-setup.sh
@@ -4,5 +4,5 @@ printf '1/3 Creating databases ♻️ \n'
 rails db:create
 printf '2/3 Migrating and correcting sequences 🐫 \n'
 rails db:migrate && rails database:correction_seq_id
-printf '2/3 Seeding with test data 🌱 \n'
+printf '3/3 Seeding with test data 🌱 \n'
 rails db:seed
