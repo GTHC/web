da302cb824d61e98cc9f285d9d639e6866b3fddb
Aman Ibrahim <amanmibra@gmail.com>
2018-11-17 17:26:12 -0500

    Updated GET shifts requests

--- app/controllers/api/v1/shifts_controller.rb
+++ app/controllers/api/v1/shifts_controller.rb
@@ -10,9 +10,11 @@ class Api::V1::ShiftsController < ApiController
 
     def set_user
       if params[:id]
-        @shift = Shift.find(params[:id])
+        # GET shift at an id within the current user's team
+        @shift = current_user.team.shifts.find(params[:id])
       else
-        @Shifts = Shift.where(user_id: current_user.id)
+        # GET all shifts under the current user's team
+        @Shifts = current_user.team.shifts
       end
     end
 end
