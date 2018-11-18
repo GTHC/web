e7c04ae7ec594c03d4b4bdacb088a692811220c3
Aman Ibrahim <amanmibra@gmail.com>
2018-11-18 14:08:28 -0500

    Replaced Shift GET views with JSON methods

--- app/controllers/api/v1/shifts_controller.rb
+++ app/controllers/api/v1/shifts_controller.rb
@@ -1,20 +1,26 @@
 class Api::V1::ShiftsController < ApiController
   before_action :set_user
+
+  # GET /api/v1/shifts/:id
+  # GET team shift at ID
   def show
-  end
-
-  def index
-  end
-
-  private
-
-    def set_user
-      if params[:id]
-        # GET shift at an id within the current user's team
-        @shift = current_user.team.shifts.find(params[:id])
-      else
-        # GET all shifts under the current user's team
-        @Shifts = current_user.team.shifts
-      end
+    @id = params[:id]
+    @shifts = current_user.team.shifts
+    if @shifts.ids.include? @id
+      render json: { status: 'SUCCESS', message: 'Shift found.', data: @shifts.find(@id) } , status: :ok
+    else
+      render json: { status: 'ERROR', message: 'ID not found.' }, status: :unprocessable_entity
     end
+  end
+
+  # GET /api/v1/shifts
+  # GET all of team shifts
+  def index
+    @shifts = current_user.team.shifts
+    if @shifts
+      render json: { status: 'SUCCESS', message: 'Shifts found.', data: @shifts } , status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Shifts not found.' }, status: :unprocessable_entity
+    end
+  end
 end
