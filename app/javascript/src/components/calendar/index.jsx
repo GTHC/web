817f36a32329cb8a64bf7373442774b8f4cf8d8a
Aman Ibrahim <amanmibra@gmail.com>
2018-11-20 22:15:04 -0500

    Change timeslots to decrease need of scrolling

--- app/javascript/src/components/calendar/index.jsx
+++ app/javascript/src/components/calendar/index.jsx
@@ -5,31 +5,19 @@ import moment from "moment";
 const localizer = Calendar.momentLocalizer(moment);
 
 class BigCal extends Component {
-  state = {
-    events: [
-      {
-        start: new Date(),
-        end: new Date(moment().add(1, "days")),
-        title: "Some title"
-      },
-      {
-        start: new Date(),
-        end: new Date(moment().add(1, "hour")),
-        title: "Some title"
-      },
-    ]
-  };
-
   render() {
-    const { team_shifts, user_shifts } = this.props.shifts;
+    const { team_shifts } = this.props.shifts;
     const events = team_shifts.map((shift) => ({
       ...shift,
       start: new Date(shift.start),
       end: new Date(shift.end),
     }));
+
     return (
-      <div className="App">
+      <div>
         <Calendar
+          step={15}
+          timeslots={8}
           localizer={localizer}
           defaultDate={new Date()}
           defaultView="day"
