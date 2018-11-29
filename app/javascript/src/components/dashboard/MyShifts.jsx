bd7d123ce8fdbfd15c915729196893518184accc
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2018-11-29 04:02:40 -0500

    Add user's shifts to dashboard Why: Make it easier to view my upcoming shifts
    This change addresses the need by:
    Added cards to represent each shift
    
    Test plan:
    - Login to GTHC
    - Navigate to calendar
    - Create a shift
    - Navigate to dashboard
    - See shift

---
+++ app/javascript/src/components/dashboard/MyShifts.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+import { Card, Header } from 'semantic-ui-react';
+
+class MyShifts extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    filterShifts = (shift) => {
+        // NOTE(anesu): We only want shifts from the next 7 days
+        const daysToAdd = 7;
+
+        const shiftStartTime = new Date(shift.start);
+        const now = new Date();
+        const sevenDaysFromNow = new Date();
+        sevenDaysFromNow.setDate(now.getDate() + daysToAdd); 
+
+
+        return shiftStartTime > now && shiftStartTime < sevenDaysFromNow;
+    }
+
+    sortByEarliestShiftsFirst = (shift1, shift2)  => {
+        return new Date(shift1.start) > new Date(shift2.start);
+    }
+
+    mapShiftToCard = (shift) => {
+        const start = moment(new Date(shift.start))
+            .calendar();
+        const end = moment(new Date(shift.end))
+            .format("h:mmA");
+        const timeTillShift = moment(new Date(shift.start))
+            .startOf(shift.start)
+            .fromNow();
+
+        return {
+            header: shift.title,
+            description: `${start} until ${end}`,
+            meta: shift.note,
+            extra: timeTillShift,
+        }
+    }
+
+    render() {
+        const items = this.props.user_shifts
+            .filter(this.filterShifts)
+            .sort(this.sortByEarliestShiftsFirst)
+            .map(this.mapShiftToCard);
+
+        return (
+           <div>
+               <Header>Upcoming Shifts</Header>
+                <Card.Group items={items} />
+           </div>       
+        );
+    }
+
+}
+
+export default MyShifts;
