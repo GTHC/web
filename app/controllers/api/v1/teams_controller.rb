2273b76cf415fad58c652ebeedb91768caa6d688
Aman Ibrahim <amanmibra@gmail.com>
2018-12-20 16:27:04 -0500

    Made API endpoint for updating team info

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TeamsController < ApiController
+  rescue_from ActionController::ParameterMissing, :with => :param_missing
+
   before_action :set_user
   def show
   end
@@ -22,6 +24,16 @@ class Api::V1::TeamsController < ApiController
     end
   end
 
+  def update
+    validate_update_params
+    if @team = Team.find(params[:id])
+      @team.update(@prime_params)
+      render json: { status: 'SUCCESS', message: 'Team updated', data: @team }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Team not found' }, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def set_user
@@ -39,5 +51,19 @@ class Api::V1::TeamsController < ApiController
       @tent_number = params[:tent_number]
       @tent_type = params[:tent_type]
       @passcode = params[:passcode]
+    end
+
+    def validate_update_params
+      params.require([:id, :name, :tent_type, :tent_number])
+      @prime_params = {
+        id: params[:id],
+        name: params[:name],
+        tent_number: params[:tent_number],
+        tent_type: params[:tent_type],
+      }
+    end
+
+    def param_missing(exception)
+      render json: { status: 'ERROR', message: exception }, status: :unprocessable_entity
     end
 end
