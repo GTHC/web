6c2263cb8dc4fb4ca7fdffa337d3c8606296a139
Aman Ibrahim <amanmibra@gmail.com>
2018-12-20 18:39:18 -0500

    Updated edit password components

--- app/javascript/src/components/user/UserProfileBody/EditPasswordPane.jsx
+++ app/javascript/src/components/user/UserProfileBody/EditPasswordPane.jsx
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-import { Button, Form } from 'semantic-ui-react';
+import { Form, Input, Dimmer } from 'semantic-ui-react';
 
 export default class UserPane extends Component {
+
+  renderNewPassword = () => (
+    <Form>
+      <Form.Input
+        id="password"
+        type="password"
+        label="Password"
+        placeholder="Password"
+      />
+      <Form.Input
+        id="password_confirmation"
+        type="password"
+        label="Password Confirmation"
+        placeholder="Password Confirmation"
+      />
+      <Form.Button>Save</Form.Button>
+    </Form>
+  );
+
+  dimmerContent = () => (
+    <Input
+      id="current_password"
+      type="password"
+      label="Current Password"
+      placeholder="Current Password"
+    />
+  );
+
   render () {
     const { user } = this.props;
 
     return (
       <div>
-        <Form>
-        <Form.Field>
-          <label>Password</label>
-          <input placeholder='Password' />
-        </Form.Field>
-        <Form.Field>
-          <label>Password Confirmation</label>
-          <input placeholder='Password Confirmation' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
-      </Form>
+        { this.renderNewPassword()}
+        <Dimmer active>
+          {this.dimmerContent()}
+        </Dimmer>
       </div>
     );
   }
