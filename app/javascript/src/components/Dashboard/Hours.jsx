5a3dacfeff9921bcbfc6b68b168a247b0288bb8a
vinitbytheminute <vinitdeveloper24@gmail.com>
2018-12-23 21:09:03 -0800

    Cleaning Up Code ( moved Dashboard into seperate function )

--- app/javascript/src/components/Dashboard/Hours.jsx
+++ app/javascript/src/components/Dashboard/Hours.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // Semantic Ui Components Used
-import { Dropdown, Card, Grid, Button, Header, Icon} from "semantic-ui-react";
+import { Dropdown, Card, Grid, Button, Header, Icon, Label} from "semantic-ui-react";
 //Endpoints not yet created, so Created Fake Data Instead
 import {getHours} from "./fakeData";
 
@@ -12,38 +12,40 @@ export default class Hours extends Component {
   super(props);
 
   this.state = {
-    Rank: 'hour',
-    Hours: getHours(),
-    Total: false //Total used to identify if User choose Week or Total View
+    rank: 'hour',
+    hours: getHours(),
+    total: false //Total used to identify if User choose Week or Total View
   };
 }
 
 
 //Function created to Switch between Total and Week View script for both Hour and Night rank
+
   hourView() {
-    return this.state.Total == false ? "Week Hours : "  : "Total Hours : " ;
+    return this.state.total == false ? "Week Hours : "  : "Total Hours : " ;
   };
 
   nightView() {
-    return this.state.Total == false ? "Week Nights : "  : "Total Nights : " ;
+    const { total } = this.state;
+    return this.state.total == false ? "Week Nights : "  : "Total Nights : " ;
   };
 
 //Function determining which View to Display
   hoursNumber(user) {
-    return this.state.Total == false ? user.hours.week : user.hours.total ;
+    return this.state.total == false ? user.hours.week : user.hours.total ;
   };
 
   nightNumber(user) {
-    return this.state.Total == false ? user.Night.week : user.Night.total ;
+    return this.state.total == false ? user.Night.week : user.Night.total ;
   };
 
 //Function determining which Rank Data to Display
   hourRank(user) {
-    return this.state.Total == false ? user.rank.whour : user.rank.thour;
+    return this.state.total == false ? user.rank.whour : user.rank.thour;
   };
 
   nightRank(user) {
-    return this.state.Total == false ? user.rank.wnight : user.rank.tnight;
+    return this.state.total == false ? user.rank.wnight : user.rank.tnight;
   };
 
 
@@ -52,43 +54,35 @@ export default class Hours extends Component {
 
 //Function determining proper coloring of badges for certain ranks
   coloring(user, view) {
-    let classes = "badge badge-"; //value of the className attribute being returned by function
     let rank;
-    if(this.state.Total){
+    if(this.state.total){
       rank = view == "hour" ? user.rank.thour : user.rank.tnight;
     }
     else {
       rank = view == "hour" ? user.rank.whour : user.rank.wnight;
     }
     if (rank == 1){
-      classes+= "warning";
+      return 'yellow';
     }
     else if (rank ==2) {
-      classes+= "secondary";
+      return 'grey';
     }
     else if (rank ==3) {
-      classes+= "dark";
+      return "brown";
     }
     else if (rank==12){
-      classes+= "danger";
+      return "red";
     }
-    console.log(classes);
-    return classes;
   };
-
-
-updateTotal = (Total) => {
-  this.forceUpdate();
-  console.log(this.state.Total);
-};
 
 
 //Function sorting Data from Greatest to Least by the given Rank and Specific View (Week or Total)
 handleNightorHours = (e, timeOfDay) => {
-  console.log(timeOfDay);
-  console.log(this.state.Total);
   let newNight; //new instance of order list used to alter State Hours value
-  if (this.state.Total) { //Order by Total Hours or Night
+  if (timeOfDay == "Time Span") {
+    return getHours();
+  }
+  else if (this.state.total && timeOfDay != "Time Span") { //Order by Total Hours or Night
     newNight = getHours().sort((b, a) => {
       const timeA = timeOfDay === 'Night'
         ? a.Night
@@ -110,17 +104,13 @@ handleNightorHours = (e, timeOfDay) => {
       return timeB.week - timeA.week;
     });
   }
-  console.log(newNight)
-  console.log(this.state.Rank);
-  console.log(this.state.Total);
-  this.setState({Rank:timeOfDay, Hours: newNight});
+  this.setState({rank:timeOfDay, hours: newNight});
 };
 
 
 //Each User Hours and Night count displayed for a given view
-  CustomCard = (props) => {
-    console.log(this.state.Total);
-    return <Card key={props.user.name} style={{width: "180px"}} centered>
+  customCard = (props) => {
+    return <Card key={props.user.name} style={{width: "180px"}}>
      <Card.Content >
        <Card.Header>{props.user.name} </Card.Header>
      </Card.Content>
@@ -134,189 +124,122 @@ handleNightorHours = (e, timeOfDay) => {
       </Card.Content>
       <Card.Content >
         <Grid.Column>
-          <div className="badge badge-info" >
+          <Label color={this.coloring(props.user, "hour")} >
             HourRank: {this.hourRank(props.user)}
-          </div>
+          </Label>
        </Grid.Column>
-       <Grid.Column className = {this.coloring(props.user, "night")}>
-         NightRank: {this.nightRank(props.user)}
+       <Grid.Column>
+        <Label color={this.coloring(props.user, "night")}>
+          NightRank: {this.nightRank(props.user)}
+        </Label>
        </Grid.Column>
       </Card.Content>
     </Card>
   }
 
 // Trying to move the dropDown options out of the render component, but dropDown fucntion  is buggy
-/**
+
 dropDown () {
-  const lengthOptions = [{
-      key: 'Week',
-      text: 'Week',
-      value: 'Week',
-      content: 'Week',
-    },
-    {
-      key: 'Total',
-      text: 'Total',
-      value: 'Total',
-      content: 'Total',
-    },
-  ];
+const lengthOptions = [{
+  key: 'Time Span',
+  text: 'Time Span ',
+  value: 'Time Span',
+  content: 'Time Span',
+},
+  {
+    key: 'Week',
+    text: 'Week',
+    value: 'Week',
+    content: 'Week',
+  },
+  {
+    key: 'Total',
+    text: 'Total',
+    value: 'Total',
+    content: 'Total',
+  },
+];
 
-  const rankOptions = [{
-      key: 'Hour',
-      text: 'Hour',
-      value: 'Hour',
-      content: 'Hour',
-    },
-    {
-      key: 'Night',
-      text: 'Night',
-      value: 'Night',
-      content: 'Night',
-    },
-  ];
-
-  return <div  className="sorting-dropdowns" style={{
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      display: 'inline-block',
-    }}
-  >
-    <div style={{display: 'inline'}}>
-      <div className="week-dropdown">
-        <div className="dropdown">
-          View by {' '}
-          <Dropdown
-            labeled
-            icon='left dropdown'
-            icon="calendar"
-            inline
-            header='Adjust time span'
-            options={lengthOptions}
-            defaultValue={lengthOptions[0].value}
-            onChange={(e, value) => {
-              const Totalv = value.value === 'Week' ? false : true;
-              console.log( Totalv, value);
-              this.setState({Total:Totalv});
-            }}
-          />
-       </div>
-      </div>
-      <br />
-       <div className="rank-dropdown">
-         <div className="dropdown">
-           Rank by {' '}
-           <Dropdown
-             labeled
-             icon="trophy"
-             inline
-             header='Adjust time span'
-             options={rankOptions}
-             defaultValue={rankOptions[0].value}
-             onChange={ (e, value) => this.handleNightorHours(e, value.value) }
-           />
-        </div>
-       </div>
+const rankOptions = [
+  {
+    key: 'Hour',
+    text: 'Hour',
+    value: 'Hour',
+    content: 'Hour',
+  },
+  {
+    key: 'Night',
+    text: 'Night',
+    value: 'Night',
+    content: 'Night',
+  },
+];
+return <div  className="sorting-dropdowns" style={{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    display: 'inline-block',
+  }}
+>
+  <div style={{display: 'inline-block'}}>
+    <div className="week-dropdown">
+      <div className="dropdown">
+        View by {' '}
+        <Dropdown
+          labeled
+          icon='left dropdown'
+          icon="calendar"
+          inline
+          options={lengthOptions}
+          defaultValue={lengthOptions[0].value}
+          onChange={(e, { value }) => {
+            const total = value === 'Week' ? false : true;
+            this.setState({ total }, () => {
+              this.handleNightorHours(e, this.state.rank);
+            });
+          }}
+        />
+     </div>
     </div>
+    <br />
+     <div className="rank-dropdown">
+       <div className="dropdown">
+         Rank by {' '}
+         <Dropdown
+           labeled
+           icon="trophy"
+           inline
+           options={rankOptions}
+           defaultValue={rankOptions[0].value}
+           onChange={ (e, value) => this.handleNightorHours(e, value.value) }
+         />
+      </div>
+     </div>
   </div>
+</div>
+
 }
-*/
 
 
   render () {
-    const lengthOptions = [{
-        key: 'Week',
-        text: 'Week',
-        value: 'Week',
-        content: 'Week',
-      },
-      {
-        key: 'Total',
-        text: 'Total',
-        value: 'Total',
-        content: 'Total',
-      },
-    ]
-
-    const rankOptions = [{
-        key: 'Hour',
-        text: 'Hour',
-        value: 'Hour',
-        content: 'Hour',
-      },
-      {
-        key: 'Night',
-        text: 'Night',
-        value: 'Night',
-        content: 'Night',
-      },
-    ]
-    const { Total, Hours } = this.state;
+    const { total, hours } = this.state;
     return (
     <Card fluid >
-      <Header textAlign='center'>
+      <Header textAlign='left'>
         <Header.Content>
-          <div style={{ paddingTop: '12px'}}>
+          <div style={{ paddingTop: '12px', paddingLeft: '15px'}} >
             <h1>Hour Breakdown</h1>
         </div>
         </Header.Content>
       </Header>
       <Card.Content textAlign="center">
-       {/*<this.dropDown />*/}
-        <div  className="sorting-dropdowns" style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            display: 'inline-block',
-          }}
-        >
-          <div style={{display: 'inline'}}>
-            <div className="week-dropdown">
-              <div className="dropdown">
-                View by {' '}
-                <Dropdown
-                  labeled
-                  icon='left dropdown'
-                  icon="calendar"
-                  inline
-                  header='Adjust time span'
-                  options={lengthOptions}
-                  defaultValue={lengthOptions[0].value}
-                  onChange={(e, value) => {
-                    const Totalv = value.value === 'Week' ? false : true;
-                    console.log( Totalv, value);
-                    this.setState({Total:Totalv}, () => {
-                      this.handleNightorHours(e,this.state.Rank);
-                    });
-                    console.log(this.state.Total);
-                  }}
-                />
-             </div>
-            </div>
-            <br />
-             <div className="rank-dropdown">
-               <div className="dropdown">
-                 Rank by {' '}
-                 <Dropdown
-                   labeled
-                   icon="trophy"
-                   inline
-                   header='Adjust time span'
-                   options={rankOptions}
-                   defaultValue={rankOptions[0].value}
-                   onChange={ (e, value) => this.handleNightorHours(e, value.value) }
-                 />
-              </div>
-             </div>
-          </div>
-        </div>
+       <this.dropDown />
       <br />
       <br />
     <Card.Group centered >
         {
-          this.state.Hours.map((user) => (
-         <this.CustomCard user={user} key={user.name}/>
+          this.state.hours.map((user) => (
+         <this.customCard user={user} key={user.name} style={{paddingRight: '12px', paddingLeft: '50px'}}/>
               ))}
       </Card.Group>
       </Card.Content>
