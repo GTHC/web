08f3b8ad404e6962c7dd8bd82b6c60f17595b7cb
Aman Ibrahim <amanmibra@gmail.com>
2018-12-25 14:43:44 -0500

    Added availability feature to UpdateShiftForm

--- app/javascript/src/components/calendar/update/UpdateShiftForm.jsx
+++ app/javascript/src/components/calendar/update/UpdateShiftForm.jsx
@@ -3,9 +3,83 @@ import React, { Component } from 'react';
 // semantic-ui
 import { Form } from 'semantic-ui-react';
 
+// utils
+import getShiftAvailability from './../utils/getShiftAvailability';
+
 class UpdateShiftForm extends Component {
 
-  state = { error: false };
+  constructor(props) {
+    super(props);
+    const usersData = props.team.data.users;
+    const userOptions = usersData.map(user => ({
+      key: user.id,
+      value: user.id,
+      text: user.name,
+    }));
+    this.state = {
+      error: false,
+      userOptions,
+    };
+  }
+
+  componentDidMount() {
+    const { start, end } = this.props.shiftData;
+    const availStart = this.hourToAvailPosition(start);
+    const availEnd = this.hourToAvailPosition(end);
+    const availDay = start.getDay();
+    getShiftAvailability(availDay, availStart, availEnd)
+    .then(res => {
+      const { data } = res.data;
+      console.log(data);
+      const userOptions = [];
+
+      // sorting users by availability and then alphabetically
+      data.sort((a, b) => {
+        if (a.shift_availability == b.shift_availability) {
+          if (a.name > b.name) { return 1; }
+          if (a.name < b.name) { return -1; }
+          return 0;
+        } else {
+          return b.shift_availability - a.shift_availability;
+        }
+      });
+
+      data.forEach(user => {
+        // choose color for availability
+        const color = user.shift_availability == 2 ? 'green' :
+        (user.shift_availability == 1 ? 'yellow' : 'red');
+
+        // adding dropdown item elements to userOptions
+        userOptions.push({
+          key: user.id,
+          value: user.id,
+          text: user.name,
+          label: { color: color, circular: true, empty: true },
+          // TODO: Add image src for user profile
+          // image: { avatar: true, src: 'https://react.semantic-ui.com/images/wireframe/image.png' },
+        });
+      });
+      this.setState({ userOptions });
+    });
+  }
+
+  /**
+   * hourToAvailPosition - get aprox. position of date in Availability component
+   *                       which runs from 7 AM - 2AM
+   * @param  {[Date]} date [Date object of either the start or end time of a shift]
+   * @param {[boolean]} isEnd [tells function if this is for a start or end]
+   * @return {[number]}      [row number in Availability grid]
+   */
+  hourToAvailPosition = (date, isEnd=false) => {
+    let output = date.getHours() - 7 + (Math.floor(date.getMinutes() / 60));
+    if (output < 0 && output > -6) {
+      output = 19;
+    } else if (output < -5) {
+      output = output + 24;
+    }
+
+    return output;
+  };
 
   onInputChange = (e, { value, id }) => {
     const { updateShiftData, shiftData } = this.props;
@@ -42,18 +116,9 @@ class UpdateShiftForm extends Component {
   render() {
     const { shiftData, team } = this.props;
     const { title, note, users } = shiftData;
+    const { userOptions } = this.state;
     // defaultValue for Dropdown
     const user_ids = users.map(user => user.id);
-    // options for Dropdown
-    const usersData = team.data.users;
-    const userOptions = usersData.map(user => ({
-      key: user.id,
-      value: user.id,
-      text: user.name,
-
-      // TODO: Add image src for user profile
-      image: { avatar: true },
-    }));
     return (
       <Form>
         <Form.Input
@@ -74,6 +139,7 @@ class UpdateShiftForm extends Component {
         <Form.Dropdown
           id="user_ids"
           fluid multiple search selection
+          scrolling upward
           label="Users on Shift"
           options={userOptions}
           defaultValue={user_ids}
