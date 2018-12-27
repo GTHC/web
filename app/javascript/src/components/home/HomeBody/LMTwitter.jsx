50b918d5a904bb7239e5a94b905edcac9c20743f
Abdulla Shahid <as737@duke.edu>
2018-12-27 01:03:26 -0500

    Countdown Widget on Card

---
+++ app/javascript/src/components/home/HomeBody/LMTwitter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Card } from 'semantic-ui-react';
+import { Timeline } from 'react-twitter-widgets';
+
+const LMTwitter = () => (
+  <Card>
+    <Card.Content>
+      <div>
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'EventsKville',
+          }}
+          options={{
+            username: 'TwitterDev',
+            height: '400',
+            width: '400',
+          }}
+        />
+      </div>
+    </Card.Content>
+  </Card>
+);
+
+export default LMTwitter;
--- app/javascript/src/components/home/HomeBody/index.jsx
+++ app/javascript/src/components/home/HomeBody/index.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import { Container, Divider, Embed, Transition } from 'semantic-ui-react';
-
+import { Image, Container, Grid ,Divider, Embed, Transition } from 'semantic-ui-react';
 import DukeTwitterTimeline from './DukeTwitterTimeline';
 import DukeVideo from './DukeVideo';
-import KvilleWeather from './KvilleWeather'
+import KvilleWeather from './KvilleWeather';
+import LMTwitter from './LMTwitter';
+import CDWidget from './CDWidget'; 
+import PointsWidget from './PointsWidget'; 
 
-export default class HomeBody extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      visible: false,
-    }
-  }
+const GridExampleVerticallyDivided = () => (
+  <Grid divided='vertically'>
+    <Grid.Row columns={1}>
+      <Grid.Column>
+        <KvilleWeather />
+      </Grid.Column>
+    </Grid.Row>
 
-  render () {
-    const { user } = this.props.userData;
-    const { visible } = this.state;
+    <Grid.Row>
+      <Grid.Column width={4}>
+        <DukeTwitterTimeline/>
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <CDWidget/>
+        <PointsWidget/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <LMTwitter/>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
 
-    return (
-      <div style={{ paddingBottom: '48px' }}>
-        <Container>
-          {/* <Container textAlign="center">
-            <Transition visible={visible} duration={1000}>
-              <h1>Welcome to your scheduler, { user.name }!</h1>
-            </Transition>
-          </Container> */}
-          {/* <Divider /> */}
-          <KvilleWeather />
-          {/* <DukeVideo /> */}
-          <DukeTwitterTimeline />
-        </Container>
-      </div>
-    );
-  }
-}
+export default GridExampleVerticallyDivided
