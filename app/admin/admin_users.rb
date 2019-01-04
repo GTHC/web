8441c88bb219eaa91ec6f305e7efc99fc57e9ef5
Aman Ibrahim <amanmibra@gmail.com>
2019-01-04 07:25:22 -0500

    Added blocks to Admin User actions depenending on current admin

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,6 +1,21 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
+  # actions
+  config.clear_action_items!
+  action_item only: :show do
+    if resource.email != current_admin_user.email
+      link_to "Delete Admin User", {action: :destroy}, method: :delete, data: { confirm: 'Admin User will be deleted forever. Are you sure?' }
+    else
+      link_to "Edit Admin User", {action: :edit}
+    end
+  end
+
+  action_item only: :index do
+    link_to "New Admin User", {action: :new}
+  end
+
+  # index page
   index do
     selectable_column
     id_column
@@ -8,7 +23,15 @@ ActiveAdmin.register AdminUser do
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
-    actions
+    column :actions do |admin|
+      raw(
+          %(
+            #{link_to 'View', admin_admin_user_path(admin)}
+            #{link_to 'Edit', edit_admin_admin_user_path(admin) unless admin != current_admin_user}
+            #{(link_to 'Delete', admin_user_path(admin), method: :delete, data: { confirm: 'Admin User will be deleted forever. Are you sure?' }) unless admin == current_admin_user }
+          )
+      )
+    end
   end
 
   filter :email
