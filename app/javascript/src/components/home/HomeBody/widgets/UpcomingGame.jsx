3bca158092c5d8439d068bed4c78885ce14ec148
Aman Ibrahim <amanmibra@gmail.com>
2019-01-08 16:04:13 -0500

    Created UpcomingGames

---
+++ app/javascript/src/components/home/HomeBody/widgets/UpcomingGame.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// semantic-ui
+import { Card, Header, Image } from 'semantic-ui-react';
+
+class UpcomingGames extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default UpcomingGames;
