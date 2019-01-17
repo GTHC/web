b02a3ab4f9d249b7d8ce276e818b5649db91feaf
Aman Ibrahim <amanmibra@gmail.com>
2019-01-17 13:23:34 -0500

    Added somewhat boolean to Avail model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_161149) do
+ActiveRecord::Schema.define(version: 2019_01_17_182216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_161149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "somewhat", default: false
   end
 
   create_table "blazer_audits", force: :cascade do |t|
