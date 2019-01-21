66fe8e79c6b3ab8089e4986336dbab650ebd1da5
Aman Ibrahim <amanmibra@gmail.com>
2019-01-21 14:20:23 -0500

    Begin transition Availability comp to calendar

--- app/javascript/src/components/availability/index.jsx
+++ app/javascript/src/components/availability/index.jsx
@@ -1,161 +1,89 @@
 import React, { Component } from 'react';
 
-// semantic-ui
-import { Grid, Segment, Divider, Button, Icon, Card } from 'semantic-ui-react';
+// calendar components
+import Calendar from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import moment from 'moment';
 
-const times = [
-  '12:00 - 1:00 am',
-  '1:00 - 2:00 am',
-  'Night Shift 🌜',
-  '7:00 - 8:00 am',
-  '8:00 - 9:00 am',
-  '9:00 - 10:00 am',
-  '10:00 - 11:00 am',
-  '11:00 - 12:00 pm',
-  '12:00 - 1:00 pm',
-  '1:00 - 2:00 pm',
-  '2:00 - 3:00 pm',
-  '3:00 - 4:00 pm',
-  '4:00 - 5:00 pm',
-  '5:00 - 6:00 pm',
-  '6:00 - 7:00 pm',
-  '7:00 - 8:00 pm',
-  '8:00 - 9:00 pm',
-  '9:00 - 10:00 pm',
-  '10:00 - 11:00 pm',
-  '11:00 - 12:00 am',
-];
+// components
+import CustomEvent from './CustomEvent';
 
-const days = [
-  'Times',
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+const localizer = Calendar.momentLocalizer(moment);
+
+const DragDropCal = withDragAndDrop(Calendar);
 
 class Availability extends Component {
+
   constructor(props) {
     super(props);
-    let grid = (new Array(7)).fill().map(() => (new Array(20).fill(2)));
-    if (props.grid !== undefined) {
-      grid = props.grid;
-    }
-
     this.state = {
-      grid: grid,
+      availabilities: props.availabilities,
     };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.renderColumns = this.renderColumns.bind(this);
   }
 
-
-  handleClick = (row, column) => {
-    if (this.props.fixed) {
+  moveEvent = ({ event, start, end, droppedOnAllDaySlot }) => {
+    if (droppedOnAllDaySlot || event.allDay) {
       return;
     }
 
-    const { updateAvailState } = this.props;
-    const setNewNumber = (val) => (val < 2 ? ++val : 0);
+    const { availabilities } = this.state;
 
-    const grid = this.state.grid;
-    const value = grid[row][column];
-    grid[row][column] = setNewNumber(value);
-    this.setState({ grid });
-    updateAvailState(grid); //updating states in higher level components
+    const idx = availabilities.indexOf(event);
+
+    const updatedEvent = { ...event, start, end };
+
+    const nextEvents = [...availabilities];
+    nextEvents.splice(idx, 1, updatedEvent);
+
+    this.setState({
+      availabilities: nextEvents,
+    });
   };
 
-  renderColumns = (col) => {
-    let data = [];
-    for (let i = 0; i < 7; i++) {
-      data.push(
-          <Grid.Column textAlign="center" key={i} onClick={() => this.handleClick(i, col)}>
-            {this.renderIcon(this.state.grid[i][col])}
-          </Grid.Column>
-      );
-    }
-
-    return data;
+  handleSelectDrag = ({ event, start, end }) => {
+    const { availabilities } = this.state;
+    const newAvailability = { start, end, title: 'test' };
+    const newAvailabilities = [...availabilities, newAvailability];
+    this.setState({
+      availabilities: newAvailabilities,
+    });
   };
-
-  renderIcon = (type) => {
-    switch (type) {
-      case 0: {
-        return (
-          <Icon color='red' name='cancel' size='large' />
-        );
-      }
-
-      case 1: {
-        return (
-          <Icon color='yellow' name='question circle' size='large' />
-        );
-      }
-
-      case 2: {
-        return (
-          <Icon color='green' name='checkmark' size='large' />
-        );
-      }
-
-      default: {
-        return (
-          <Icon color='red' name='cancel' size='large' />
-        );
-      }
-    }
-  };
-
-  renderKey = () => (
-    <div>
-      <Grid relaxed="very" padded>
-        <Grid.Row columns={3}>
-          <Grid.Column textAlign="center">
-            <h4> Unavailable </h4> <Icon color='red' name='cancel' size='large' />
-          </Grid.Column>
-          <Grid.Column textAlign="center">
-            <h4> Somewhat Available </h4> <Icon color='yellow' name='question circle' size='large' />
-          </Grid.Column>
-          <Grid.Column textAlign="center">
-            <h4> Available </h4> <Icon color='green' name='checkmark' size='large' />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={1}>
-          <Grid.Column textAlign="center">
-            <i>Click icons below to select availability.</i>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-    </div>
-  );
 
   render() {
+    const { availabilities } = this.state;
+    const events = availabilities.map(avail => ({
+      ...avail,
+      title: avail.somewhat ? 'Somewhat' : 'Available',
+      start: new Date(avail.start),
+      end: new Date(avail.end),
+    }));
+
+    const components = {
+      event: CustomEvent,
+    };
+
     return (
-      <div>
-        {this.renderKey()}
-        <Grid columns='equal' celled >
-          <Grid columns={8} padded>
-          {days.map(day => (
-            <Grid.Column textAlign="center" key={day} color='green'>
-              <Grid.Row style={{ height: '10%' }}>{day}</Grid.Row>
-            </Grid.Column>
-          ))}
-          </Grid>
-          {times.map((time, row) => (
-            <Grid.Row key={time} style={{ height: '6.5%' }}>
-              <Grid.Column> {time} </Grid.Column>
-              {this.renderColumns(row)}
-            </Grid.Row>
-          ))}
-        </Grid>
-      </div>
+      <DragDropCal
+        resizeable
+        // components={components}
+        onEventDrop={this.moveEvent}
+        onEventResize={this.moveEvent}
+        step={30}
+        timeslots={4}
+        selectable
+        popup
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="day"
+        events={events}
+        // onSelectEvent={this.onSelectEvent}
+        onSelectSlot={this.handleSelectDrag}
+        // eventPropGetter={}
+        style={{ height: '80vh' }}
+      />
     );
   }
+
 }
 
 export default Availability;
--- app/javascript/src/components/user/UserProfileBody/EditUserAvailability.jsx
+++ app/javascript/src/components/user/UserProfileBody/EditUserAvailability.jsx
@@ -6,49 +6,14 @@ import Availability from '../../availability/index';
 class EditUserAvailability extends Component {
   constructor (props) {
     super(props);
-    this.state = {
-      availability: props.user.availability,
-      disabled: true,
-      savePressed: false,
-      loading: props.userState.isLoading,
-    };
   }
-
-  updateAvailState = availability => this.setState({
-    availability,
-    disabled: false,
-    savePressed: false,
-  });
-
-  onSave = () => {
-    const { updateAvailability } = this.props;
-    const { availability } = this.state;
-    updateAvailability({ availability });
-    this.setState({ savePressed: true, disabled: true });
-  };
 
   render () {
     const { user } = this.props;
-    const { availability, disabled, savePressed, loading } = this.state;
     return (
-      <div>
-        <Availability
-          {...this.props}
-          updateAvailState={this.updateAvailState}
-          grid={user.availability}
-        />
-        <Button disabled={disabled} onClick={this.onSave}>Save</Button>
-        {
-          !loading && savePressed &&
-          <Message
-            positive
-            attached
-            icon="check"
-            header="Updated Successfully!"
-            content="Your availability information has been changed."
-          />
-        }
-      </div>
+      <Availability
+        availabilities={user.availabilities}
+      />
     );
   }
 }
