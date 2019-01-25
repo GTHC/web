530f2eeff9d269673d42b2cebe0e3af55cf5eab0
Aman Ibrahim <amanmibra@gmail.com>
2019-01-25 08:16:16 -0500

    Added dropdown color support for new availability

--- app/javascript/src/components/calendar/update/UpdateShiftForm.jsx
+++ app/javascript/src/components/calendar/update/UpdateShiftForm.jsx
@@ -5,6 +5,7 @@ import { Form } from 'semantic-ui-react';
 
 // utils
 import PopupInfo from './../utils/PopupInfo';
+import getTeamAvailability from '../utils/getTeamAvailability';
 
 // images
 import * as defaultSrc from './../../../images/default_image.png';
@@ -30,15 +31,52 @@ class UpdateShiftForm extends Component {
     const props = this.props;
     if (prevProps.start_time !== props.start_time || prevProps.end_time !== props.end_time) {
       // TODO: Call API to get team availability
-      // this.getAvailabilities();
+      this.getAvailabilities(props.start_time, props.end_time);
     }
   }
 
   componentDidMount() {
     // Before the form mounts, we get all of the users availabilities for the possible shift
     // TODO: Call API to get team availability
-    // this.getAvailabilities();
+    const { start_time, end_time } = this.props;
+    this.getAvailabilities(start_time, end_time);
   }
+
+  getAvailabilities = (start, end) => {
+    getTeamAvailability(start, end)
+    .then(res => {
+      const { data } = res.data;
+      const userOptions = [];
+
+      // sorting users by availability and then alphabetically
+      data.sort((a, b) => {
+        if (a.level == b.level) {
+          if (a.name > b.name) { return 1; }
+          if (a.name < b.name) { return -1; }
+          return 0;
+        } else {
+          return b.level - a.level;
+        }
+      });
+
+      data.forEach(user => {
+        let src = defaultSrc;
+        if (user.avatarURL) {
+          src = user.avatarURL;
+        }
+
+        // adding dropdown item elements to userOptions
+        userOptions.push({
+          key: user.id,
+          value: user.id,
+          text: user.name,
+          label: { color: user.color, circular: true, empty: true },
+          image: { src: src, rounded: true },
+        });
+      });
+      this.setState({ userOptions });
+    });
+  };
 
 
   onInputChange = (e, { value, id }) => {
