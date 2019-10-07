ce6e42dec9d07c618395f45b97afcec4c037d3d2
Aman Ibrahim <amanmibra@gmail.com>
2019-10-06 21:00:07 -0400

    Added netid migration to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # TODO(Aman): Properly remove devise and its attributes from User model
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :team, optional: true
 
   has_many :user_shifts, dependent: :destroy
