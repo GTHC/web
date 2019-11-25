2e3dd80a0ce986df610edf0e0cb8c73092e2e714
Mike Liu <mikelxc@Mikes-MacBook-Pro.local>
2019-11-24 21:03:47 -0500

    Added OneSignalSDK

---
+++ app/assets/javascripts/OneSignalSDK.js
@@ -0,0 +1,7 @@
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+var OneSignal = window.OneSignal || [];
+              OneSignal.push(function() {
+                OneSignal.init({
+                  appId: "3d625d7e-2f51-4214-95a6-dcfd85a12f63",
+                })
+              });
