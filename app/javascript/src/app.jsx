3082af1982e01c96721106520c68e8db8c129127
Aman Ibrahim <amanmibra@gmail.com>
2019-12-03 14:01:50 -0500

    Removed unused imports from src/app.jsx

--- app/javascript/src/app.jsx
+++ app/javascript/src/app.jsx
@@ -2,17 +2,8 @@ import React, { Component } from 'react';
 
 // redux tools
 import { Provider } from 'react-redux';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
-import thunkMiddleware from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
-
-// redux dev tools
-import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
 
 // redux persist
-import storage from 'redux-persist/es/storage';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
 // containers
