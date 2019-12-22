9caa00f91d0e3d2a9a2ceea196fad188c69bf22f
ssh50 <ssh50@duke.edu>
2019-12-21 20:18:00 -0600

    Access netid from notif accept page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def current_user
+    if params[:mobile]
+      token = OAuth2::AccessToken.new(oauth_client, params[:token])
+      user_info = JSON.parse(token.get('/oidc/userinfo').body)
+      @current_user = User.find_or_create_by_oauth(user_info)
+      @current_user
+    else
+      @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+      @current_user
+    end
+  end
+
+  helper_method :current_user
+
 end
