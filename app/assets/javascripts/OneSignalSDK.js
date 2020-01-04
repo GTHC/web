ca586e48c308b1069fa00a053b2ea8f22c723500
ssh50 <ssh50@duke.edu>
2020-01-03 20:24:09 -0600

    Remove old javascript files

--- app/assets/javascripts/OneSignalSDK.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js')
-  var OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "b290fd9a-eedf-44b0-8bfd-6a37646957b6",
-    });
-  });
