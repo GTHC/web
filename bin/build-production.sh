b07de15340c688ea8cb87d26b48ff1bf134ad576
Aman Ibrahim <amanmibra@gmail.com>
2020-01-26 01:26:25 -0500

    Made fixes to docker running issues

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -1,3 +1,4 @@
+docker-compose build --no-cache
 docker-compose run -e NODE_ENV=production app rails assets:precompile
 heroku container:push web --app gthc-web
 heroku container:release web --app gthc-web
