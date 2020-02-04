d5ae6e66b05ffa944de47896cd9be03909aea00c
ssh50 <ssh50@duke.edu>
2020-02-04 15:28:03 -0500

    Update scripts

--- bin/build-staging.sh
+++ bin/build-staging.sh
@@ -1,4 +1,4 @@
-docker-compose build --no-cache
+docker-compose build
 docker-compose run -e NODE_ENV=production app rails assets:precompile
 heroku container:push web --app gthc-web-staging
 heroku container:release web --app gthc-web-staging
